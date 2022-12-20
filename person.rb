8d4c679d8b06885def803c3016b34e494299339d
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-20 09:43:28 +0000

    Remove classes and import needed files into the Person file

--- person.rb
+++ person.rb
@@ -7,16 +7,9 @@
 # Private method is_of_age? that returns true if @age is greater or equal to 18 and false otherwise.
 # Public method can_use_services? that returns true if person is of age or if they have permission from parents.
 
-# Interface
-# Create a class Nameable.
-# Implement a method called correct_name that will raise a NotImplementedError.
-
-class Nameable
-  def correct_name
-    raise NotImplementedError, 'Please implement the Nameable#correct_name method'
-  end
-end
-
+require_relative 'nameable.rb' 
+require_relative 'capitalize_decorator.rb'
+require_relative 'trimmer_decorator.rb'
 # Make sure that your Person class inherits from Nameable
 class Person < Nameable
   attr_accessor :name, :age
@@ -44,44 +37,6 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
-  end
-end
-
-# Prepare base Decorator
-# Make sure that it inherits from Nameable.
-# In the constructor assign a nameable object from params to an instance variable.
-# Implement the correct_name method that returns the result of the correct_name method of the @nameable.
-
-class BaseDecorator < Nameable
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-# Prepare CapitalizeDecorator and TrimmerDecorator
-# For the CapitalizeDecorator:
-# Create a class that inherits from the base Decorator class.
-class CapitalizeDecorator < BaseDecorator
-  # Implement a method correct_name that capitalizes the output of @nameable.correct_name.
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-# For the TrimmerDecorator:
-# Create a class that inherits from the base Decorator class.
-class TrimmerDecorator < BaseDecorator
-  # Implement a method correct_name that makes sure that the output of @nameable.correct_name
-  #  has a maximum of 10 characters. If it's longer it should trim the word.
-  def correct_name
-    correct_name = @nameable.correct_name
-    correct_name = correct_name.slice(0, 10) if correct_name.length > 10
-    correct_name
   end
 end
 
