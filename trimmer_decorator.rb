ccb768b0900304e92f760a6b95488fbbe00545d0
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-20 09:43:47 +0000

    Create a file to hold to the trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,12 @@
+require_relative 'base_decorator.rb'
+# For the TrimmerDecorator:
+# Create a class that inherits from the base Decorator class.
+class TrimmerDecorator < BaseDecorator
+  # Implement a method correct_name that makes sure that the output of @nameable.correct_name
+  #  has a maximum of 10 characters. If it's longer it should trim the word.
+  def correct_name
+    correct_name = @nameable.correct_name
+    correct_name = correct_name.slice(0, 10) if correct_name.length > 10
+    correct_name
+  end
+end
