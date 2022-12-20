aa65a5fc79797b2eb6b5485bf57f47bc131d1961
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-20 09:46:40 +0000

    Fix errors

--- person.rb
+++ person.rb
@@ -15,8 +15,7 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def
-      initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
