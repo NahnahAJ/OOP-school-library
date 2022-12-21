a3640d0f1d5f60bfe5997196648a3768ffaa677f
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-21 04:54:37 +0000

    Create the belongs-to side (a student belongs to a classroom).

--- student.rb
+++ student.rb
@@ -3,6 +3,9 @@
 # Constructor extends parent's constructor by adding @classroom and a parameter for it.
 # Method play_hooky that returns "¯\(ツ)/¯".
 
+require_relative 'classroom.rb'
+require_relative 'person.rb'
+
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
