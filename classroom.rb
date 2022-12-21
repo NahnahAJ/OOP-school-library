698697fdf68b406f867f07d6c9cf18918fce1504
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-21 04:53:58 +0000

    Create the has-many side (a classroom has many students).

---
+++ classroom.rb
@@ -0,0 +1,11 @@
+class Classroom
+    attr_accessor :label
+    def initialize(label)
+      @label = label
+      @classroom = []
+    end
+
+    def add_student(classroom, student)
+      @classroom.push(student) unless classroom.include?(student)
+    end    
+end
