81b5c0eca12034d2c8c4db4342c2a034e512e0f2
nahnahAJ <nahnahahjuwah@gmail.com>
2023-01-05 13:53:45 +0000

    Fix all linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,31 +2,31 @@ require_relative 'spec_helper'
 
 describe Book do
   before :each do
-    @book = Book.new("Title", "Author")
+    @book = Book.new('Title', 'Author')
   end
 
-  describe "#new" do
-    it "when two parameters are provided and the book is created" do
+  describe '#new' do
+    it 'when two parameters are provided and the book is created' do
       expect(@book).to be_an_instance_of Book
     end
   end
 
-  describe "#title" do
-    it "expects the title of the book to be the same" do
-      expect(@book.title).to eql "Title"
+  describe '#title' do
+    it 'expects the title of the book to be the same' do
+      expect(@book.title).to eql 'Title'
     end
   end
 
-  describe "#author" do
-    it "expects the author of the book to be the same" do
-      expect(@book.author).to eql "Author"
+  describe '#author' do
+    it 'expects the author of the book to be the same' do
+      expect(@book.author).to eql 'Author'
     end
   end
 
-  describe "#add_rental" do
-  let(:person) { double('person', rentals:[]) }
-  let(:date) { double('date') }
-    it "expects an instance of rental to be created" do
+  describe '#add_rental' do
+    let(:person) { double('person', rentals: []) }
+    let(:date) { double('date') }
+    it 'expects an instance of rental to be created' do
       expect(@book.add_rental(person, date)).to be_an_instance_of(Rental)
     end
 
@@ -35,6 +35,4 @@ describe Book do
       expect(@book.rentals).to include(rental)
     end
   end
-
-  
 end
