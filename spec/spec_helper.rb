04b50b0d749728c3a51260d7c20e6b204890cda0
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-05 20:05:24 +0430

    Add relative path for ruby classes in spec_helper.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require_relative '../book'
 require_relative '../person'
 require_relative '../student'
+require_relative '../teacher'
+require_relative '../rental'
+require_relative '../classroom'
 require 'yaml'
